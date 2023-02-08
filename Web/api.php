73923cbc9e9a105bbace8e0bf4b77aa61eea92db
SoleilDivin <vinesh.sinel@gmail.com>
2023-02-08 21:06:44 +0300

    fix: allow users to select multiple photos to upload at once - change the form input to support multiple files uploading at the same time - change api.php to support multiple picture uploading - change results to show previews of every images

--- Web/api.php
+++ Web/api.php
@@ -11,27 +11,30 @@
  */
 header("Content-type:application/json;charset=utf-8");
 $file = $_FILES['file'];
-$name = $file['name'];
-$tmp_name = $file['tmp_name'];
-$type = strtolower(substr($name,strrpos($name,'.')+1)); 
-if(!is_uploaded_file($file['tmp_name'])){
-    return ;
-}
+$names = $file['name'];
+$tmp_names = $file['tmp_name'];
+$src = [];
+for($i=0; $i < count($tmp_names); $i++) {
+    $type = strtolower(substr($names[$i], strrpos($names[$i], '.') + 1));
+    if (!is_uploaded_file($tmp_names[$i])) {
+        return;
+    }
     $ch = curl_init();
     $url = 'https://telegra.ph/upload';
-    $post_data = array('file' => new \CURLFile(realpath($tmp_name)));
+    $post_data = array('file' => new \CURLFile(realpath($tmp_names[$i])));
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1); //POST提交
-    curl_setopt($ch, CURLOPT_POSTFIELDS,$post_data);
-    $data =curl_exec($ch);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+    $data = curl_exec($ch);
     curl_close($ch);
-    $res= json_decode($data,TRUE);
-    $src = $res[0]['src'];
+    $res = json_decode($data, TRUE);
+    $src[] = 'https://missuo.ru'.$res[0]['src'];
+}
     $result = array(
         'code' => 200,
         'status' => 'success',
-        'src' => 'https://missuo.ru'.$src
+        'src' => $src
         );
     $result
     = json_encode($result,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
